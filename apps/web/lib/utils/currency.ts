757443e87422fc35f710405944b027d681563f87
santialemarino <santialemarino@gmail.com>
2026-07-12 16:51:01 -0300

    refactor: delegate formatamount to the formatvalue core

--- apps/web/lib/utils/currency.ts
+++ apps/web/lib/utils/currency.ts
@@ -1,6 +1,6 @@
 import cc from 'currency-codes';
 
-import { getLocaleTag } from '@/lib/utils/locale';
+import { formatValue } from '@/lib/utils/format';
 
 // All currencies with exchange rate support.
 const SUPPORTED_CONVERSION_CURRENCIES = new Set(['USD', 'ARS', 'BRL', 'EUR', 'GBP']);
@@ -14,9 +14,5 @@ export function formatAmount(value: string, locale?: string, currency?: string):
   const num = Number(value);
   if (isNaN(num)) return value;
   const digits = currency ? (cc.code(currency)?.digits ?? 2) : 2;
-  const hasDecimals = num % 1 !== 0;
-  return new Intl.NumberFormat(getLocaleTag(locale), {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: hasDecimals ? digits : 0,
-  }).format(num);
+  return formatValue(num, { locale, maxDecimals: digits });
 }
